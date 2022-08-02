bb6f40db83d11d83f6067f9f8ad56e09fcd46a0f
bernaljg <bernaljg@berkeley.edu>
2022-08-02 13:09:02 -0400

    Update filename

--- src/augment_umls_test.sh
+++ src/augment_umls_test.sh
@@ -5,4 +5,4 @@
 
 #source myconda
 #mamba activate rw_uva1
-python augment_UMLS.py /data/Bodenreider_UMLS_DL/UMLS_VERSIONS 2020AB /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/data/test_original_auis.p /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/data/test_new_auis.p 200 /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output 4 scui scui_lui scui_cui cambridgeltl/SapBERT-from-PubMedBERT-fulltext
+python augment_UMLS.py /data/Bodenreider_UMLS_DL/UMLS_VERSIONS 2020AB /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/data/vishesh_dataset_subset_original_auis.p /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/data/vishesh_dataset_subset_new_auis.p 200 /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output 4 scui scui_lui scui_cui cambridgeltl/SapBERT-from-PubMedBERT-fulltext
