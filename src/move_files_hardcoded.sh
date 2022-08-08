13c6d0158c00e2768f99ef762d8d988f8d34a73e
bernaljg <bernaljg@berkeley.edu>
2022-08-08 18:37:40 -0400

    Debug

--- src/move_files_hardcoded.sh
+++ src/move_files_hardcoded.sh
@@ -14,18 +14,42 @@ cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-wa
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AB-two-way/dev.tsv
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AB-two-way/test.tsv
 
+#For training datasets obtained from 2020AA only, still use rw-uva-2020AB dev and test datasets
+
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/one-way.train.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-no-gold/train.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-no-gold/dev.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-no-gold/test.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-no-gold/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-no-gold/test.tsv
 
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/two-way.train.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-no-gold/train.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-no-gold/dev.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-no-gold/test.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-no-gold/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-no-gold/test.tsv
 
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/one-way.train.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way/train.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way/dev.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way/test.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way/test.tsv
 
 cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/two-way.train.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way/train.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way/dev.tsv
-cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way/test.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way/test.tsv
+
+#Creating directory which uses a training dataset which is obtained from 2020AA only but small scale and extremely similar to the 2020AB dev and test datasets
+mkdir /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train
+mkdir /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train-no-gold
+mkdir /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train
+mkdir /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train-no-gold
+
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train-no-gold/train.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train-no-gold/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train-no-gold/test.tsv
+
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/0/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train-no-gold/train.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train-no-gold/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/0/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train-no-gold/test.tsv
+
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train/train.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/one-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/one-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-one-way-small-train/test.tsv
+
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/3/classifiers/1/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train/train.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.dev.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train/dev.tsv
+cp /data/Bodenreider_UMLS_DL/Interns/Bernal/rw_uva/output/2/classifiers/1/two-way.test.tsv /data/Bodenreider_UMLS_DL/Interns/Bernal/few-shot-bioIE/data/rw-uva-2020AA-two-way-small-train/test.tsv
